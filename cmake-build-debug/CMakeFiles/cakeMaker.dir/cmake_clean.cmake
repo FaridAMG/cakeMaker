5b88d7913d67bcbd5280f840502b52a03e3f15d8
Unknown <faridmarin25@gmail.com>
2018-08-21 23:52:52 -0600

    CreatedInitialTemplate
    Created the main, from here well branch to our respective parts of project

---
+++ cmake-build-debug/CMakeFiles/cakeMaker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cakeMaker.dir/main.cpp.o"
+  "cakeMaker.pdb"
+  "cakeMaker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cakeMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
